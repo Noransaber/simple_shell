61b2e777dbb2ac1e8cf3990dbdd8339df1030357
Noransaber <noransaber685@gmail.com>
2023-05-24 04:23:01 -0700

    source code foe pipline functions

---
+++ pipe_functions_one.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+/**
+ * handle_error - handle error of piplines
+ * @message: meesage error
+ * Return: nothing, void
+**/
+void handle_error(const char *message)
+{
+	fprintf(stderr, "%s\n", message);
+}
+/**
+ * close_pipes - close the pipes
+ * @pipefd: file describtor for the pipe
+ * @num_pipes: number of pipes
+ * Return: nothing
+**/
+void close_pipes(int pipefd[][2], int num_pipes)
+{
+	for (int i = 0; i < num_pipes; i++)
+	{
+		close(pipefd[i][0]);
+		close(pipefd[i][1]);
+	}
+}
+/**
+ * wait_for_children - handle waiting for child proccess
+ * @num_commands: take number of commands
+ * Return: nothing
+**/
+void wait_for_children(int num_commands)
+{
+	for (int i = 0; i < num_commands; i++)
+	{
+		wait(NULL);
+	}
+}
---
+++ pipline_function_two.c
@@ -0,0 +1,42 @@
+#include "shell.h"
+/**
+ * create_pipes - pipes creator function.
+ * @pipefd: container to store the fd of |
+ * @num_pipes: how many numbers of | tp create
+ * Return: if we did it =>0 on success,if not =>1 on failure
+ */
+int create_pipes(int pipefd[][2], int num_pipes)
+{
+	for (int i = 0; i < num_pipes; i++)
+	{
+		if (pipe(pipefd[i]) == -1)
+		{
+			perror("pipe");
+			return (1);
+		}
+	}
+	return (0);
+}
+/**
+ * free_commands - free memory.
+ * @commands: commandds array
+ * @num_commands: count of commands
+ * Return: nooooooothing
+ */
+void free_commands(char *commands[], int num_commands)
+{
+	for (int i = 0; i < num_commands; i++)
+	{
+		free(commands[i]);
+	}
+}
+/**
+ * my_pipline_handler - pipline  cmd handler.
+ * @pipeline:string of pipline  cmds
+ * @envp: env var array
+ * Return: 0 on success, 1 on failure
+ */
+int my_pipline_handler(char *pipeline, char *envp[])
+{
+	return (run_shell(pipeline, envp));
+}

5ad7820414179fdc23ff3cb3f631855838750ab1
TenIke <anthonydzani@gmail.com>
2023-05-26 04:28:48 -0700

    updating

--- fock_execute.c
+++ fock_execute.c
@@ -8,7 +8,7 @@
 pid_t fork_execute_function(char **array_string, char *env[])
 {
 	pid_t my_child_pid;
-	int waiting_status;
+	int waiting_status = 0;
 
 	my_child_pid = fork();
 	if (my_child_pid < 0)
@@ -16,20 +16,22 @@ pid_t fork_execute_function(char **array_string, char *env[])
 		perror("Error: Fork failed");
 		return (-1);
 	}
-		if (my_child_pid == 0)
-		{
-			if (execve(array_string[0], array_string, env) == -1)
+	else if (my_child_pid == 0)
+	{
+		if (execve(array_string[0], array_string, env) == -1)
 		{
 			fprintf(stderr, "%s: No such file or directory\n", array_string[0]);
 			_exit(EXIT_FAILURE);
 		}
-	}
-		else
+	}	
+	else
 	{
-	if (wait(&waiting_status) == -1)
-	{
-		perror("Error: Wait failed");
+		if (wait(&waiting_status) == -1)
+		{	
+			perror("Error: Wait failed");
+		}
+			
+		return WEXITSTATUS(waiting_status);
 	}
-	}
-	return WEXITSTATUS(waiting_status);	
+	return (-1);
 }
